e1a0eb71778be8a1e1ecca5acbcc84b98f0172af
Edem <edem228@gmail.com>
2015-11-21 23:57:08 +0100

    formatage des dates des events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,8 +1,9 @@
 class EventsController < ApplicationController
 	before_action :authenticate_user!, except: :index
 	before_action :find_envent, only: [:edit, :show, :update]
+
 	def index
-		@events = Event.order(:event_date).all.reverse
+		@events = Event.order(:event_date).all
 	end
 	
 	def new
@@ -19,6 +20,7 @@ class EventsController < ApplicationController
 	end
 	def edit
 	end
+
 	def update
 		if @event.update(event_params)
 			redirect_to @event
@@ -29,26 +31,24 @@ class EventsController < ApplicationController
 
 	def show
 		@user = current_user
-		@event = Event.find(params[:id])
 		@participants = @event.participants
-
 	end
 
 	def participate
 		Participation.create(user_id: current_user.id, event_id: params[:id])
 		redirect_to participate_event_path(params[:id])
 	end
+
 	def price
-		@price = Participation.cost / @event.participations.count
-
+		@price = Event.cost / @event.participations.count
 	end
-
 
 	private
 	
 	def find_envent
 		@event = Event.find(params[:id])
 	end
+
 	def event_params
 		params.require(:event).permit(:name, :venue, :description, :address, :hour, :event_date, :places, :user_id)
 	end
